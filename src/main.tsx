033052d7b42e7a827cef23860ce10bc15e2bd952
Clinton Ngotta <clintonngotta@gmail.com>
2025-06-11 03:16:14 +0300

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import StoreProvider from "./store/StoreProvider.tsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<StoreProvider>
 			<App />
 		</StoreProvider>
+		<Analytics />
 	</StrictMode>
 );
